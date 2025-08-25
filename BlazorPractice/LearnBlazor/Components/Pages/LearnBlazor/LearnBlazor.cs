36f487a0c0c750a5c4b3284860ccecc69c7bc8be
Suman Chava <Suman.Chava+ghub@blueyonder.com>
2025-08-25 15:33:55 +0530

    Added one way binding example

---
+++ BlazorPractice/LearnBlazor/Components/Pages/LearnBlazor/LearnBlazor.cs
@@ -0,0 +1,14 @@
+﻿using LearnBlazor.Models;
+
+namespace LearnBlazor.Components.Pages.LearnBlazor
+{
+    public partial class LearnBlazor
+    {
+        Product product = new Product()
+        {
+            Id = 1,
+            Name = "Test",
+            IsActive = true,
+        };
+    }
+}
---
+++ BlazorPractice/LearnBlazor/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
